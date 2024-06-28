7b6bde72ded0a8f0927f44e551c6ac8985853889
Adan Rodriguez <adan.rodriguez.fusta@gmail.com>
2024-06-27 22:21:21 -0300

    Mejoro sección de trámites

---
+++ src/utils/data.js
@@ -0,0 +1,42 @@
+export const TRAMITES = [
+  {
+    image: {
+      link: "patente.avif",
+      alt: "Boleta de patente",
+    },
+    link: {
+      href: "https://www.santafe.gov.ar/e-pt-boletas",
+      label: "Patente",
+    },
+  },
+  {
+    image: {
+      link: "inmobiliario.avif",
+      alt: "Boleta de impuesto inmobiliario",
+    },
+    link: {
+      href: "https://www.santafe.gov.ar/e-in-boletas",
+      label: "Impuesto inmobiliario",
+    },
+  },
+  {
+    image: {
+      link: "epeoficinavirtual.avif",
+      alt: "Persona utlizando la Oficina Virtual de la EPE",
+    },
+    link: {
+      href: "https://www.epe.santafe.gov.ar/oficina-virtual",
+      label: "EPE - Oficina Virtual",
+    },
+  },
+  {
+    image: {
+      link: "partidadenacimiento.avif",
+      alt: "Una partida de nacimiento",
+    },
+    link: {
+      href: "https://www.santafe.gov.ar/rcdigital",
+      label: "Partida de nacimiento",
+    },
+  },
+];
